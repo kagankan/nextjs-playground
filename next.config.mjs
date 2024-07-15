99531efd0631a946b983e61921c604222e021d52
kagan <intend.on.games+git@gmail.com>
2024-07-16 04:03:27 +0900

    fix url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: isProd
+    ? "https://nextjs-playground-app--nextjs-playground-b1ad1.us-central1.hosted.app"
+    : "",
+};
 
 export default nextConfig;
