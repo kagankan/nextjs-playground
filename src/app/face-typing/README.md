1ac72b3e9ea12fffcbf0796d98e496dc80363ef8
kagan <intend.on.games+git@gmail.com>
2024-07-30 03:14:04 +0900

    サーバーのエラー修正

---
+++ src/app/face-typing/README.md
@@ -0,0 +1,4 @@
+## memo
+
+`@tensorflow-models/face-landmarks-detection` の依存解決に失敗するため `npm i --force` が必要。
+yarn にしたほうがいいかも
--- src/app/face-typing/page.tsx
+++ src/app/face-typing/page.tsx
@@ -1,5 +1,11 @@
 import { Metadata } from "next";
-import FaceDirectionDetector from "./_components/FaceDirectionDetector";
+import dynamic from "next/dynamic";
+
+// navigatorでエラーになるため dynamic import
+const FaceDirectionDetector = dynamic(
+  () => import("./_components/FaceDirectionDetector"),
+  { ssr: false }
+);
 
 export const metadata: Metadata = {
   title: "顔の向きで文字入力",
