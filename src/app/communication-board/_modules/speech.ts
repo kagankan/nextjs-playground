339892c22af353a46ed817e7f2d608166ff166c2
kagan <intend.on.games+git@gmail.com>
2024-08-03 21:55:33 +0900

    fix build

--- src/app/communication-board/_modules/speech.ts
+++ src/app/communication-board/_modules/speech.ts
@@ -1,7 +1,10 @@
-const synth = window.speechSynthesis;
-const voices = synth.getVoices();
-
 export const speak = (text: string) => {
+  if (typeof window === "undefined" || !window.speechSynthesis) {
+    return;
+  }
+  const synth = window.speechSynthesis;
+  const voices = synth.getVoices();
+
   const utterance = new SpeechSynthesisUtterance(text);
   utterance.voice = voices[0];
   utterance.pitch = 1.5;
